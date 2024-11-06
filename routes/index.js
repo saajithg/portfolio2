649c1c2ede4fa7ce182e540fc6d6349155244aec
Saajith Gnanakanthan <sg@Saajiths-Air-2.ht.home>
2024-11-06 02:06:54 -0500

    Initial commit

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-// Define routes for each page
-router.get('/', (req, res) => res.render('home', { title: 'Home' }));
-router.get('/about', (req, res) => res.render('about', { title: 'About Me' }));
-router.get('/projects', (req, res) => res.render('projects', { title: 'Projects' }));
-router.get('/contact', (req, res) => res.render('contact', { title: 'Contact Me' }));
+router.get('/', (req, res) => {
+    res.render('index', { title: 'Saajith Gnanakanthan Portfolio' });
+});
 
 module.exports = router;
